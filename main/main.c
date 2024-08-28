149c3edc0fbe8eec5ac16a6b46595f75155d6b12
EliasFIlho <eliasfilho202@gmail.com>
2024-08-28 14:34:38 -0300

    fixed IP

--- main/main.c
+++ main/main.c
@@ -14,15 +14,11 @@
 // #define PIN_CS 15
 // #define PIN_INT 4
 
-
-
 #define PIN_MISO 19
 #define PIN_MOSI 23
 #define PIN_SCLK 18
 #define PIN_CS 5
 #define PIN_INT 4
-
-
 
 #define SPI_HOST SPI2_HOST
 
@@ -39,8 +35,7 @@ static void eth_event_handler(void *arg, esp_event_base_t event_base, int32_t ev
     case ETHERNET_EVENT_CONNECTED:
         esp_eth_ioctl(eth_handle, ETH_CMD_G_MAC_ADDR, mac_addr);
         ESP_LOGI(TAG, "Ethernet Link Up");
-        ESP_LOGI(TAG, "Ethernet HW Addr %02x:%02x:%02x:%02x:%02x:%02x",
-                 mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+        ESP_LOGI(TAG, "Ethernet HW Addr %02x:%02x:%02x:%02x:%02x:%02x", mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
         break;
     case ETHERNET_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "Ethernet Link Down");
@@ -59,7 +54,14 @@ static void eth_event_handler(void *arg, esp_event_base_t event_base, int32_t ev
 static void got_ip_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
 {
     ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
-    ESP_LOGI(TAG, "Got IP Address: " IPSTR, IP2STR(&event->ip_info.ip));
+    const esp_netif_ip_info_t *ip_info = &event->ip_info;
+
+    ESP_LOGI(TAG, "Ethernet Got IP Address");
+    ESP_LOGI(TAG, "~~~~~~~~~~~");
+    ESP_LOGI(TAG, "ETHIP:" IPSTR, IP2STR(&ip_info->ip));
+    ESP_LOGI(TAG, "ETHMASK:" IPSTR, IP2STR(&ip_info->netmask));
+    ESP_LOGI(TAG, "ETHGW:" IPSTR, IP2STR(&ip_info->gw));
+    ESP_LOGI(TAG, "~~~~~~~~~~~");
 }
 
 esp_err_t w5500_ethernet_init(esp_eth_handle_t *handle)
@@ -74,23 +76,23 @@ esp_err_t w5500_ethernet_init(esp_eth_handle_t *handle)
     };
 
     ESP_ERROR_CHECK(spi_bus_initialize(SPI_HOST, &busfc, SPI_DMA_CH_AUTO));
-    
+
     eth_mac_config_t mac_config = ETH_MAC_DEFAULT_CONFIG();
     eth_phy_config_t phy_config = ETH_PHY_DEFAULT_CONFIG();
     phy_config.reset_gpio_num = -1;
-    phy_config.phy_addr = 1;
+    phy_config.phy_addr = -1;
     spi_device_interface_config_t spi_devcfg = {
         .mode = 0,
-        .clock_speed_hz = 16*1000*1000,
+        .clock_speed_hz = 16 * 1000 * 1000,
         .spics_io_num = PIN_CS,
         .queue_size = 10,
     };
 
     eth_w5500_config_t w5500_config = ETH_W5500_DEFAULT_CONFIG(SPI_HOST, &spi_devcfg);
-    //w5500_config.int_gpio_num = PIN_INT;
+    // w5500_config.int_gpio_num = PIN_INT;
     w5500_config.int_gpio_num = -1;
     w5500_config.spi_host_id = SPI_HOST;
-    w5500_config.poll_period_ms = 5;  
+    w5500_config.poll_period_ms = 5;
     esp_eth_mac_t *mac = esp_eth_mac_new_w5500(&w5500_config, &mac_config);
     esp_eth_phy_t *phy = esp_eth_phy_new_w5500(&phy_config);
     esp_eth_config_t eth_config = ETH_DEFAULT_CONFIG(mac, phy);
@@ -100,6 +102,7 @@ esp_err_t w5500_ethernet_init(esp_eth_handle_t *handle)
 void app_main(void)
 {
 
+    uint8_t mac_addr[6] = {0xA0, 0xDD, 0x6C, 0x03, 0x09, 0xC0};
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
     {
@@ -112,18 +115,28 @@ void app_main(void)
         ESP_ERROR_CHECK(ret);
     }
 
+    ESP_ERROR_CHECK(esp_netif_init());
+
     esp_eth_handle_t eth_handle = NULL;
     ESP_ERROR_CHECK(w5500_ethernet_init(&eth_handle));
 
-    ESP_ERROR_CHECK(esp_netif_init());
-
     esp_event_loop_create_default();
-    esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL);
     esp_netif_config_t iface_config = ESP_NETIF_DEFAULT_ETH();
     esp_netif_t *iface = esp_netif_new(&iface_config);
-    ESP_ERROR_CHECK(esp_netif_attach(iface, esp_eth_new_netif_glue(eth_handle)));
+    ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL));
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_netif_attach(iface, esp_eth_new_netif_glue(eth_handle)));
+
+    esp_netif_ip_info_t ip_info;
+    esp_netif_set_ip4_addr(&ip_info.ip, 10, 109, 49, 105);      // Replace with an appropriate IP address for your network
+    esp_netif_set_ip4_addr(&ip_info.gw, 10, 109, 49, 1);        // Replace with your gateway IP
+    esp_netif_set_ip4_addr(&ip_info.netmask, 255, 255, 255, 0); // Replace with your subnet mask
+    ESP_ERROR_CHECK(esp_netif_dhcpc_stop(iface));
+    ESP_ERROR_CHECK(esp_netif_set_ip_info(iface, &ip_info));
+
     ESP_ERROR_CHECK(esp_eth_start(eth_handle));
+    ESP_ERROR_CHECK(esp_eth_ioctl(eth_handle, ETH_CMD_S_MAC_ADDR, mac_addr));
+
     // Next steps - Test event loops handles and ethernet interface
     // Try do OTA with the ethernet interface
 }
